00a39bdec78eba90f858fef2d2b2b0771ad753cf
Marco Manunta <mar.manunta@gmail.com>
2024-02-23 22:08:32 +0100

    create layout, homepage, and styles

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import '../assets/styles/globals.css'
+
+export const metadata = {
+  title: 'Property Pulse | Find The Perfect Rental',
+  description: 'Find Your Dream Rental Property',
+  keywords: 'rental, find rental, find properties',
+}
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  )
+}
+
+export default MainLayout
